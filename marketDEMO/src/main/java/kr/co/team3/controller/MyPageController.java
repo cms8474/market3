37f984fac010fa3ec9b2a8e598283e5fc9baa430
mincheolkang34 <mincheolkang34@gmail.com>
2025-09-26 19:30:20 +0900

    마이페이지 구현 중

---
+++ marketDEMO/src/main/java/kr/co/team3/controller/MyPageController.java
@@ -0,0 +1,12 @@
+package kr.co.team3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyPageController {
+    @GetMapping("/my/home")
+    public String home(){
+        return "my/home";
+    }
+}
