14e7e28e0b53c6e41137f535d8dee0abd8a5fd78
wjsdydwns01 <113407599+weasdf12@users.noreply.github.com>
2025-10-16 17:07:43 +0900

    Weasdf (#41)
    버전 페이지

--- marketDEMO/src/main/java/kr/co/team3/controller/admin/AdminConfigController.java
+++ marketDEMO/src/main/java/kr/co/team3/controller/admin/AdminConfigController.java
@@ -17,7 +17,7 @@ import java.nio.file.StandardCopyOption;
 import java.util.*;
 
 @Controller
-@RequestMapping("kmarket/admin/config")
+@RequestMapping("/admin/config")
 public class AdminConfigController {
     private final VersionService versionService;
 
