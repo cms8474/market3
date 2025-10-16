3eb149f52d4cf98904c1e3a1fb6165bf1c3f5f87
MincheolKang34 <MincheolKang34@gmail.com>
2025-10-16 17:26:39 +0900

    Feature/mincheolkang (#42)
    * 마이페이지 구현 중
    
    * 마이페이지 메인 구현
    
    * home.html 오타 수정
    
    * home.html 수정, 주문상세 모달 창 작업 중
    
    * 모달창 띄우기 구현
    
    * 주문상세, 판매자정보 모달 구현
    
    * 문의하기 모달창 구현
    
    * 마이페이지 구현 중
    
    * 마이페이지 메인 구현 완료
    
    * 마이페이지 스타일 수정
    
    * 마이페이지 사이드바 링크 수정, 전체주문내역 페이지 추가
    
    * 마이페이지 메인 스크립트 변수명 변경, css 분리
    
    * 마이페이지 전체주문정보, 포인트내역, 쿠폰내역 페이지 조회
    
    * 나의리뷰, 문의하기, 나의설정 구현
    
    * 약관 페이지 구현 중
    
    * 약관 프론트 구현
    
    * 충돌 해결
    
    * 회사소개 메인 구현
    
    * 회사소개 메인 css 수정
    
    * rename file
    
    * 헤더 링크 수정, 회사소개-기업문화 구현
    
    * 회사소개-소식과이야기&채용 구현
    
    * 회사소개-미디어 구현, 회사소개 스타일 분리
    
    * 회사소개 푸터 내용 및 스타일 수정
    
    * 마이페이지 메인 링크 수정
    
    * 마이페이지 백엔드 구현 중
    
    * 마이페이지 최근주문내역 구현 중
    
    * 회사소개 스타일 수정, 내용 추가
    
    * 회사소개 이미지 추가
    
    * 불필요 태그 제거
    
    * 약관 아이콘 추가
    
    * 병합 시도
    
    * 마이페이지 home 최근주문내역 구현 중
    
    * 마이페이지 최근주문내역 수정, 주문상세 모달 구현 중
    
    * sql 수정, 마이페이지 주문상세 구현 중
    
    * 잘못된 병합 수정
    
    * 마이페이지 주문상세 모달 구현 중
    
    * 모달 창 위치 조정
    
    * 마이페이지 주문상세 모달 창 구현 완료
    
    * 리뷰 작성 구현중
    
    ---------
    
    Co-authored-by: mins <c5092ms@gmail.com>

--- marketDEMO/src/test/java/kr/co/team3/mapper/ProductOrderMapperTest.java
+++ marketDEMO/src/test/java/kr/co/team3/mapper/ProductOrderMapperTest.java
@@ -1,6 +1,7 @@
 package kr.co.team3.mapper;
 
 import kr.co.team3.dto.my.ProductOrderDTO;
+import kr.co.team3.mapper.my.ProductOrderMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
